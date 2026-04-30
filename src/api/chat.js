3900b0eaebe588293109b4b35cc274ce6ca11669
sssiion <rta2752775@gmail.com>
2026-04-30 15:49:36 +0900

    Update chat.js

--- src/api/chat.js
+++ src/api/chat.js
@@ -1,7 +1,7 @@
 import api from './index';
 
-export const sendMessage = (storeId, question) =>
-  api.post(`/api/chat/${storeId}`, { question });
+export const sendMessage = (storeId, question, workerName) =>
+  api.post(`/api/chat/${storeId}`, { question, workerName });
 
 export const getChatLogs = (storeId) =>
   api.get(`/api/chat/${storeId}/logs`);
