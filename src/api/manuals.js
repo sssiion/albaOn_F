34ae59fb5172db89b032bd08cc16aaa387d059b9
sssiion <rta2752775@gmail.com>
2026-04-30 17:37:32 +0900

    Update manuals.js

--- src/api/manuals.js
+++ src/api/manuals.js
@@ -2,6 +2,7 @@ import api from './index';
 
 export const getManuals   = (storeId)             => api.get(`/api/manuals/${storeId}`);
 export const createManual = (storeId, data)        => api.post(`/api/manuals/${storeId}`, data);
+export const updateManual = (storeId, manualId, data)      => api.put(`/api/manuals/${storeId}/${manualId}`, data);
 export const deleteManual = (storeId, manualId)    => api.delete(`/api/manuals/${storeId}/${manualId}`);
 
 export const uploadAudio = (storeId, formData) =>
