abfc40319d3f58af562ff942fce0a8736bfe3946
sssiion <rta2752775@gmail.com>
2026-04-30 18:06:42 +0900

    Update categories.js

--- src/api/categories.js
+++ src/api/categories.js
@@ -3,3 +3,5 @@ import api from './index';
 export const getCategories    = (storeId)              => api.get(`/api/categories/${storeId}`);
 export const addCategory      = (storeId, name)        => api.post(`/api/categories/${storeId}`, { name });
 export const deleteCategory   = (storeId, categoryId)  => api.delete(`/api/categories/${storeId}/${categoryId}`);
+export const updateCategory = (storeId, categoryId, name) =>
+  api.put(`/api/categories/${storeId}/${categoryId}`, { name });
