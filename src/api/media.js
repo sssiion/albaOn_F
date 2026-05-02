3f66a05bce1ce95b4610b2faa5b5fb1ed8427d49
sssiion <rta2752775@gmail.com>
2026-05-02 20:13:55 +0900

    Update media.js

--- src/api/media.js
+++ src/api/media.js
@@ -7,3 +7,10 @@ export const uploadMedia = (manualId, formData) =>
 
 export const deleteMedia = (mediaId) =>
   api.delete(`/api/media/${mediaId}`);
+export const uploadNodeMedia = (manualId, formData) =>
+  api.post(`/api/media/node/${manualId}`, formData, {
+    headers: { 'Content-Type': 'multipart/form-data' }
+  });
+
+export const deleteNodeMedia = (mediaId) =>
+  api.delete(`/api/media/node/${mediaId}`);
